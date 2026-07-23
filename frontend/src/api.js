9d1a8a8719bd0803d2bc55336aa419cd32fa0cfe
Sanjay <25beece145@kahedu.edu.in>
2026-07-23 12:30:09 +0530

    Update API URL to Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://event-management-system-1wtm.onrender.com/api',
 })
 
 API.interceptors.request.use((req) => {
